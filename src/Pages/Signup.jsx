05719caf74ba0f8196397fed2e20bddc91cc718e
Captain-Jack-Sparrow-BP <arhamjain042008@gmail.com>
2026-09-04 23:18:53 +0530

    feat: add signup page

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react" 
+
+function SignUp(){
+
+    const [formData, setFormData] = useState({
+        name:"",
+        email:"",
+        password:"",
+        number:"",
+        confirmPassword:""
+    })
+
+    function HandleSubmit(event){
+        event.preventDefault()
+        console.log(formData)
+        setFormData({
+            name:"",
+            username:"",
+            email:"",
+            password:"",
+            number:"",
+            confirmPassword:""
+        })
+    }
+
+    function HandleChange(event){
+        const {name,value} = event.target
+
+        setFormData(prev => ({
+            ...prev,
+            [name]:value
+        }))
+    }
+
+
+    return(
+        <> 
+        <main className="login-page">
+
+            <div className="login-l">
+                <div className="l-logo"><Link to="/">URBAN FLOW</Link></div>
+                
+
+                <div className="l-text">
+                    <p>Move the <br></br> city forward.</p>
+                    <span>A living network for the people, places, and resources that keep cities moving.</span>
+                </div>
+            </div>
+
+
+            <div className="login-r">
+                <div className="form-head">
+                    <p>Create your UrbanFlow account</p>
+                 </div>
+                <form onSubmit={HandleSubmit}>
+                    <p>Name</p>
+                    <input name="name" value={formData.name} onChange={HandleChange} placeholder="Name"></input>
+                    <p>Username</p>
+                    <input name="username" value={formData.username} onChange={HandleChange} placeholder="Username"></input>
+                    <p>E-mail</p>
+                    <input name="email" value={formData.email} onChange={HandleChange} placeholder="E-mail"></input>
+                    <p>Password</p>
+                    <input name="password" value={formData.password} onChange={HandleChange} placeholder="Password"></input>
+                    <p>Phone Number</p>
+                    <input name="number" value={formData.number} onChange={HandleChange} placeholder="Phone Number"></input>
+                    <p>Confirm Password</p>
+                    <input name="confirmPassword" value={formData.confirmPassword} onChange={HandleChange} placeholder="Confirm Password"></input>
+                </form>
+
+                <div className="form-bottom">
+                    <button onClick={HandleSubmit}>Submit</button>
+                    <p>Don't Have a account<span>Sign Up!</span> </p>
+                </div>
+            </div>
+        </main>
+        </>
+    )
+}
+
+export default SignUp
