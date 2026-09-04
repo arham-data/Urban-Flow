eaf5132e818a6f5a2140626d97af3ad5454e08e3
Captain-Jack-Sparrow-BP <arhamjain042008@gmail.com>
2026-09-04 23:09:51 +0530

    feat: add authentication pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Landing_page from "./Pages/Landing_page.jsx"
 import "./Pages/Landing_page.css"
+import Login from "./Pages/Login.jsx"
+import "./Pages/Login_Signup.css"
 
 function App() { 
 
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react" 
+
+function Login(){
+
+    const [formData,setFormData] = useState({
+        username:"",
+        password:""
+    })
+
+    function HandleChange(event){
+        const {name,value} = event.target
+
+        setFormData(prev =>({
+            ...prev,
+            [name]:value
+        }))
+    }
+
+    function HandleSubmit(event){
+        event.preventDefault()
+        console.log(FormData)
+        setFormData({
+            username: "",
+            password:""
+        })
+    }
+
+
+    return(
+        <main className="login-page">
+            <div className="login-l">
+                <div className="l-logo">URBAN FLOW</div>
+
+                <div className="l-text">
+                    <p>Move the <br></br> city forward.</p>
+                    <span>A living network for the people, places, and resources that keep cities moving.</span>
+                </div>
+            </div>
+
+
+            <div className="login-r">
+                <div className="form-head">
+                    <p>Create your UrbanFlow account</p>
+                 </div>
+                <form onSubmit={HandleSubmit}>
+                    <p>Username</p>
+                    <input name="username" value={formData.username} onChange={HandleChange} placeholder="Username"></input>
+                    <p>Password</p>
+                    <input name="password" value={formData.password} onChange={HandleChange} placeholder="Password"></input>
+                    
+                </form>
+
+                <div className="form-bottom">
+                    <button onClick={HandleSubmit}>Submit</button>
+                    <p>Already Have an account<span>Login</span></p>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Login
